3793cf236a29d5f09133c6a347117d2bf41635c6
Corstiaan <ch@fluxmatix.com>
2018-09-16 01:20:28 +0200

    dump

--- Fluxmatix.AspNetCore.TagHelpers.QuillEditor/QuillEditorTagHelper.cs
+++ Fluxmatix.AspNetCore.TagHelpers.QuillEditor/QuillEditorTagHelper.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Fluxmatix.AspNetCore.TagHelpers.QuillEditor
 {
-    public class QuillEditorTagHelper
+    [HtmlTargetElement("quill-editor")]
+    public class QuillEditorTagHelper : TagHelper
     {
+        QuillEditorOptions _options;
+
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression Content { get; set; }
+
+        public QuillEditorTagHelper(QuillEditorOptions options)
+        {
+            _options = options;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if(Content.Model != null)
+            {
+                output.TagName = "";
+
+                var attrStr = "";
+
+                foreach(var attr in context.AllAttributes)
+                {
+                    if(attr.Name != "asp-for")
+                        attrStr += attr.Name + $"=\"{attr.Value}\" ";
+                }
+
+                output.Content.AppendHtmlLine($"<div id=\"quill-editor\" {attrStr}></div>");
+                output.Content.AppendHtmlLine($"<input type=\"hidden\" id=\"quill-editor-buffer\" name=\"{Content.Name}\" value=\"{WebUtility.HtmlEncode(Content.Model.ToString())}\" />");
+            }
+        }
     }
 }
