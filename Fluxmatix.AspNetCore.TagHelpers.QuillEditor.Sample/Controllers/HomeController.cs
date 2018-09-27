6e984fbef6226fc0fb063358c1fa5e7770dfaa8b
Corstiaan <ch@fluxmatix.com>
2018-09-27 16:37:57 +0200

    Added sample web project for Quill editor

---
+++ Fluxmatix.AspNetCore.TagHelpers.QuillEditor.Sample/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fluxmatix.AspNetCore.TagHelpers.QuillEditor.Sample.Models;
+
+namespace Fluxmatix.AspNetCore.TagHelpers.QuillEditor.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("multipleeditors")]
+        public IActionResult MultipleEditors()
+        {
+            return View();
+        }
+
+        [HttpPost("showcontent")]
+        public IActionResult ShowContent(SampleModel model)
+        {
+            return Content(model.Content1, "text/html");
+        }
+
+        [HttpPost("showcontentmultiple")]
+        public IActionResult ShowContentMultiple(SampleModel model)
+        {
+            var html = "<p>Content 1:</p>" + model.Content1;
+            html += "<p>Content 2:</p>" + model.Content2;
+            html += "<p>Content 3:</p>" + model.Content3;
+
+            return Content(html, "text/html");
+        }
+    }
+}
---
+++ Fluxmatix.AspNetCore.TagHelpers.QuillEditor.Sample/Models/SampleModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Fluxmatix.AspNetCore.TagHelpers.QuillEditor.Sample.Models
+{
+    public class SampleModel
+    {
+        public string Content1 { get; set; }
+        public string Content2 { get; set; }
+        public string Content3 { get; set; }
+    }
+}
